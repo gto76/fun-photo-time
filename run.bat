0dd038f12a63f904a795b084425324746f2b8001
Jure Sorn <sornjure@gmail.com>
2015-03-07 16:58:10 +0100

    added run.bat

---
+++ run.bat
@@ -0,0 +1,20 @@
+echo off
+
+SET mainClass='si.gto76.funphototime.FunPhotoTime' 
+SET appName='fun-photo-time'
+SET scriptName=%~n0
+
+echo %scriptName%: COMPILING...
+mkdir target\main
+dir /s /B *.java > sources.txt
+javac -d target\main @sources.txt
+del sources.txt
+cd target
+
+echo %scriptName%: PACKAGING IN JAR...
+jar cvfe %appName%.jar %mainClass% -C ..\src\main resources -C main .
+
+echo %scriptName%: EXECUTING JAR...
+java -jar %appName%.jar
+
+cd ..
